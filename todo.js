12a723b51c7efd64cc8191b35e0bd54bff1b08a8
​m-sathish-varma < sathishvarma@ideas2it.com>
2019-09-25 15:02:15 +0530

    font and comment added

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
 var tasks = [];
-var taskInfo = [];
 var activeSubTask = [];
 
 document.getElementById("menu-icon").addEventListener("click", function(){
@@ -9,14 +8,14 @@ document.getElementById("menu-icon").addEventListener("click", function(){
     var words = getElementByClassName("menu-name");
     if (optionValue.value === "openMenu") {
         leftDiv.style.width = "22.5%";
-        middle.style.width = "56%";
+        middle.style.width = "77%";
         optionValue.value = "closeMenu";
         for (let i in words) {
             words[i].style.display = "block";
         }
     } else {
         leftDiv.style.width = "50px";
-        middle.style.width = "72%";
+        middle.style.width = "97%";
         optionValue.value = "openMenu";
         for (let i in words) {
             words[i].style.display = "none";
@@ -27,9 +26,11 @@ document.getElementById("menu-icon").addEventListener("click", function(){
 document.getElementById("plus-symbol").addEventListener("click", function(){
     let newList = getElementById("new-list");
     var leftDiv = getElementById("left-column");
+    var middle = getElementById("middle-column");
     var optionValue = getElementById("option-value");
     var words = getElementByClassName("menu-name");
     leftDiv.style.width = "22.5%";
+    middle.style.width = "77%";
     optionValue.value = "closeMenu";
     newList.focus();
     for (let i in words) {
@@ -48,6 +49,50 @@ document.getElementById("new-list").addEventListener("keyup", function(event){
         }
     }
 });
+
+function addNewList(inputValue) {
+       let subTask = new Array();
+       let right = getElementById("right-column");
+       let tasksInformation = new Array();
+       let sideMenuContent = getElementById("created-list");
+       let newDiv = document.createElement("div");
+       let newSpan = document.createElement("span");
+       right.style.display = "none";
+       right.style.transition = "0.3s";
+       newDiv.style.display = "flex";
+       newDiv.style.color = "#3d71e4";
+       newDiv.innerHTML = "<i class = 'fa fa-list-ul'></i>";
+       newSpan.className = "menu-name";
+       newDiv.id = getId();
+       tasksInformation.id = newDiv.id;
+       tasksInformation.subTask = subTask;
+       newSpan.style.display = "block";
+       newSpan.style.color = "black";
+       newSpan.innerHTML = checkTaskTitle(inputValue.value);
+       tasksInformation.taskName = newSpan.innerHTML;
+       newDiv.appendChild(newSpan);
+       newDiv.style.height = "37px";
+       newDiv.style.paddingLeft = "17px";
+       newDiv.style.cursor = "pointer";
+       newDiv.style.alignItems = "center";
+       newDiv.addEventListener("click", function(){
+           var taskName = getElementById("task-name");
+           taskName.innerHTML = newSpan.innerHTML;
+           displayTaskInfo(this.id);
+       });
+       sideMenuContent.appendChild(newDiv);
+       displayTaskName(newSpan.innerHTML);
+       inputValue.value = "";
+       tasks.push(tasksInformation);
+}
+
+function displayTaskName(inputValue) {
+    let subTaskList = getElementById("sub-task-list");
+    subTaskList.innerHTML = "";
+    makeInputEmpty("new-task");
+    let taskName = getElementById("task-name");
+    taskName.innerHTML = inputValue;
+}
 
 document.getElementById("new-task").addEventListener("keyup", function(event) {
 let inputValue = getElementById("new-task");
@@ -73,7 +118,8 @@ function addNewSubTask() {
     let taskSpan = document.createElement("span");
     let subTaskList = getElementById("sub-task-list");
     taskSpan.innerHTML = newTask.value;
-    taskSpan.style.marginLeft = "6%";
+    newDiv.innerHTML = "<i class='fa fa-circle-o'></i>";
+    taskSpan.style.marginLeft = "4%";
     newDiv.id = getId();
     newDiv.style.cursor = "pointer";
     newDiv.setAttribute("class", "new-task");
@@ -87,61 +133,24 @@ function addNewSubTask() {
     subTaskList.appendChild(newDiv);
     subTaskInfo = taskSelected.subTask;
     subTaskInfo.push(newSubTask);
+    makeInputEmpty("new-task");
     newTask.focus();
 }
 
 document.getElementById("plus").addEventListener("click", function(){
     let subTaskList = getElementById("sub-task-list");
-    subTaskList.innerHTML = "";
     let newTask = getElementById("new-task");
+    makeInputEmpty("new-task");
     newTask.focus();
 });
 
-function addNewList(inputValue) {
-       let subTask = new Array();
-       let tasksInformation = new Array();
-       let sideMenuContent = getElementById("created-list");
-       let newDiv = document.createElement("div");
-       let newSpan = document.createElement("span");
-       newDiv.style.display = "flex";
-       newDiv.style.color = "#3d71e4";
-       newDiv.innerHTML = "<i class = 'fa fa-list-ul'></i>";
-       newSpan.className = "menu-name";
-       newDiv.id = getId();
-       tasksInformation.id = newDiv.id;
-       tasksInformation.subTask = subTask;
-       newSpan.style.display = "block";
-       newSpan.style.color = "black";
-       newSpan.innerHTML = inputValue.value;
-       tasksInformation.taskName = newSpan.innerHTML;
-       newDiv.appendChild(newSpan);
-       newDiv.style.height = "37px";
-       newDiv.style.paddingLeft = "17px";
-       newDiv.style.cursor = "pointer";
-       newDiv.style.alignItems = "center";
-       newDiv.addEventListener("click", function(){
-           var taskName = getElementById("task-name");
-           taskName.innerHTML = newSpan.innerHTML;
-           displayTaskInfo(this.id);
-       });
-       sideMenuContent.appendChild(newDiv);
-       displayTaskName(inputValue);
-       inputValue.value = "";
-       tasks.push(tasksInformation);
-}
-
-function displayTaskName(inputValue) {
-    let subTaskList = getElementById("sub-task-list");
-    subTaskList.innerHTML = "";
-    makeInputEmpty("new-task");
-    let taskName = getElementById("task-name");
-    taskName.innerHTML = inputValue.value;
-}
-
 function displayTaskInfo(idValue) {
+    let right = getElementById("right-column");
     var taskSelected = tasks.find(function(event) {
         return event.id == idValue;
     });
+    right.style.display = "none";
+    right.style.transition = "0.3s";
     console.log(taskSelected);
     let subTaskInfo = taskSelected.subTask;
     let subTaskList = getElementById("sub-task-list");
@@ -150,8 +159,9 @@ function displayTaskInfo(idValue) {
     function displaySubTask(subTask, index) {
         let newDiv = document.createElement("div");
         let taskSpan = document.createElement("span");
+        newDiv.innerHTML = "<i class='fa fa-circle-o' aria-hidden='true'></i>";
         taskSpan.innerHTML = subTask.subTaskName;
-        taskSpan.style.marginLeft = "6%";
+        taskSpan.style.marginLeft = "4%";
         newDiv.setAttribute("class", "new-task");
         newDiv.id = subTask.id;
         newDiv.style.cursor = "pointer";
@@ -175,15 +185,17 @@ if (13 == event.keyCode) {
 });
 
 function addNewStep() {
+    let notes = getElementById("add-note-content");
     let step = getElementById("new-step");
     let stepInfo = [];
-//    let notes = getElementbyId("add-notes");
     let steps = activeSubTask.steps
     stepInfo.stepName = step.value;
-//    stepInfo.notes = notes.value;
     let newDiv = document.createElement("div");
     let newSpan = document.createElement("span");
     let steplist = getElementById("steps-list");
+    newDiv.innerHTML = "<i class='fa fa-circle-o' aria-hidden='true'></i>";
+    activeSubTask.notes = notes.innerHTML;
+    newSpan.style.paddingLeft = "1rem";
     newSpan.innerHTML = step.value;
     newDiv.appendChild(newSpan);
     newDiv.id = getId();
@@ -191,29 +203,6 @@ function addNewStep() {
     steplist.appendChild(newDiv);
     stepInfo.id = newDiv.id;
     steps.push(stepInfo);
-}
-
-function displayExistingSteps() {
-    let subTaskName = getElementById("sub-task-name");
-    subTaskName.innerHTML = selectedSubTask.subTaskName;
-    let steplist = getElementById("steps-list");
-    steplist.innerHTML = "";
-    console.log("step");
-    let steps = activeSubTask.steps;
-    steps.forEach(displaySteps);
-    function displaySteps(step, index) {
-    console.log("step inside");
-        let newDiv = document.createElement("div");
-        let taskSpan = document.createElement("span");
-        taskSpan.innerHTML = step.stepName;
-        taskSpan.style.marginLeft = "6%";
-        newDiv.setAttribute("class", "new-task");
-        newDiv.id = step.id;
-        newDiv.style.cursor = "pointer";
-        newDiv.append(taskSpan);
-        steplist.appendChild(newDiv);
-    }
-    
 }
 
 function addSubTaskSteps(subTaskId) {
@@ -229,7 +218,55 @@ function addSubTaskSteps(subTaskId) {
             }
         }
     }
+    let right = getElementById("right-column");
+    right.style.display = "block";
+    right.style.transition = "0.3s";
     displayExistingSteps();
+}
+
+function displayExistingSteps() {
+    let subTaskName = getElementById("sub-task-name");
+    let notes = getElementById("add-note-content");
+    subTaskName.innerHTML = selectedSubTask.subTaskName;
+    let steplist = getElementById("steps-list");
+    steplist.innerHTML = "";
+    console.log("step");
+    if (notes.innerHTML && notes) { 
+        notes.innerHTML = activeSubTask.notes; 
+    }
+    let steps = activeSubTask.steps;
+    steps.forEach(displaySteps);
+    function displaySteps(step, index) {
+    console.log("step inside");
+        let newDiv = document.createElement("div");
+        let taskSpan = document.createElement("span");
+        newDiv.innerHTML = "<i class='fa fa-circle-o' aria-hidden='true'></i>";
+        taskSpan.innerHTML = step.stepName;
+        taskSpan.style.marginLeft = "6%";
+        newDiv.setAttribute("class", "new-task");
+        newDiv.id = step.id;
+        newDiv.style.cursor = "pointer";
+        newDiv.append(taskSpan);
+        steplist.appendChild(newDiv);
+    }
+    
+}
+
+function checkTaskTitle(title) {
+   console.log(title);
+   var list = tasks.filter(function(task) {
+       if(task.taskName.includes("(")) {
+           return title === task.taskName.slice(0, task.taskName.indexOf("("));
+       } else {
+           return task.taskName == title;
+       }
+   });
+   let size = list.length;
+   if(0 == size) {
+       return title;
+   } else {
+       return (title+"&nbsp;("+size+")");
+   }
 }
 
 function makeInputEmpty(inputId) {
